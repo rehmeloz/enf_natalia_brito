d0f95e9cafe39efb073ae0c4a6970176cc1dcb7b
rehmeloz <renatamelo.pro@outlook.com>
2026-07-08 16:19:26 -0300

    Adiciona display do número de WhatsApp

--- enf_natalia_brito.client/src/constants/links.ts
+++ enf_natalia_brito.client/src/constants/links.ts
@@ -1,4 +1,5 @@
 export const WHATSAPP_NUMBER = "5513996232284";
+export const WHATSAPP_DISPLAY = "(13) 99623-2284";
 export const WHATSAPP_MESSAGE = "Olá! Gostaria de saber mais!";
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
 
