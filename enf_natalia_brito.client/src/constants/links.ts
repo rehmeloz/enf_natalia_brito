86ba0791fb3b7d8a65d5ce7bc68dc1a5389d11ed
rehmeloz <renatamelo.pro@outlook.com>
2026-07-13 16:49:15 -0300

    Adiciona suporte a variáveis de ambiente e corrige .gitignore

--- enf_natalia_brito.client/src/constants/links.ts
+++ enf_natalia_brito.client/src/constants/links.ts
@@ -1,7 +1,7 @@
-export const WHATSAPP_NUMBER = "5513996232284";
-export const WHATSAPP_DISPLAY = "(13) 99623-2284";
+export const WHATSAPP_NUMBER = import.meta.env.VITE_WHATSAPP_NUMBER;
+export const WHATSAPP_DISPLAY = import.meta.env.VITE_WHATSAPP_DISPLAY;
 export const WHATSAPP_MESSAGE = "Olá! Gostaria de saber mais!";
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
 
-export const INSTAGRAM_HANDLE = "nataliabritoclinic";
+export const INSTAGRAM_HANDLE = import.meta.env.VITE_INSTAGRAM_HANDLE;
 export const INSTAGRAM_LINK = `https://instagram.com/${INSTAGRAM_HANDLE}`;
